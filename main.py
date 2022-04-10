0187d94321dbdd50216fb0958499d32632609508
Hajaro <Hajaro@users.noreply.github.com>
2022-04-10 12:56:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+morse_code = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+}
+converted_text = ""
+text_to_convert = input("Please input text to convert into morse code: ")
+for character in text_to_convert:
+    try:
+        converted_text = converted_text + morse_code[character.lower()]
+        converted_text = converted_text + ","
+    except:
+        converted_text = converted_text + "invalid character"
+        converted_text = converted_text + ","
+print(f"Your text in morse code is {converted_text[:-1]}")
+
